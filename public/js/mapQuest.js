57786a93a9fb5162fafb129db39ea39794f8c68f
thomastangerine <thomastang.thomas97@gmail.com>
2020-02-27 22:34:50 -0800

    finish lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+    L.mapquest.key = "GCCGXG7OvANBHJBkHnVbgScYr0B4AlJO";
+
+    var map = L.mapquest.map('map', {
+        center: [32.879047, -117.235944],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12,
+        zoomControl: false
+    });
+
+    L.marker([32.879047, -117.235944]).addTo(map);
 }
